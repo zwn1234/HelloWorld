7b84d5f9a334946a93ba30595fcb5ca6a24bd7cb
Zyis Wiscovitch <zyis@nycstudents.net>
2026-09-17 23:22:16 -0400

    added scanner and class.txt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("What's your name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
+        File f = new File("Class.txt");
+        s = new Scanner(f);
+        String course = s.nextLine();
+        System.out.println("Welcome to " + course + "!");
     }
 }
